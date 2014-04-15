d09406a0cadaad6f2d1426f0309ba31acd1c2c68
amakovetskiy <amakovetskiy@amakovetkiy-Lenovo-G700>
2014-04-15 17:28:26 +0400

    inital commit

---
+++ lsways/PluginLswaysclass.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Запрещаем напрямую через браузер обращение к этому файлу.
+ */
+if (!class_exists('Plugin')) {
+    die('Hacking attemp!');
+}
+
+class PluginLsways extends Plugin {
+
+    public function Activate() {
+        return true;
+    }
+
+    public function Deactivate(){
+    	return true;
+    }
+
+    public function Init() {    	
+		return true;
+    }
+}
+?>
